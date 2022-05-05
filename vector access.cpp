cb1b3c7f5cd6bfb3d0afbad2c94cf40013dc3769
Muskan kumari <86469435+Muskankumari2002@users.noreply.github.com>
2022-05-05 12:26:56 +0530

    at() functrion
    To acesss the element in vector at function is used

---
+++ vector access.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+int main()
+{
+    // at() function is used to access the element
+    vector<int>num{1,2,3,4,5};
+    cout<<"element at 0 index: "<<num.at(0)<<endl;
+    cout<<"element at 3 index: "<<num.at(3)<<endl;
+    return 0;
+}

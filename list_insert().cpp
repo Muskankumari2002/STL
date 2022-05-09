e0d8fb1ce9bd2e6ca9714954470454e4e279d59e
Muskan kumari <86469435+Muskankumari2002@users.noreply.github.com>
2022-05-09 21:51:51 +0530

    insert 2 element in list

---
+++ list_insert().cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<list>
+using namespace std;
+int main()
+{
+    list<string> li ={"c is a langauge"};
+    list<string>::iterator itr=li.begin();
+    li.insert(itr,2,"java");
+    for(itr=li.begin();itr != li.end();++itr)
+    {cout<<*itr;
+    }
+    return 0;
+}
